26488c0bf99e91b6403782a0add6776ef60eaff8
agibalovsu <agibalovsu@mail.ru>
2022-06-01 23:44:06 +0300

    change wrong test to model answer

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -4,5 +4,5 @@ RSpec.describe Answer, type: :model do
   it { should validate_presence_of :body }
 
   it { should belong_to :question }
-  it { should validate_presence_of :question_id }
+  it { should have_db_index :question_id }
 end
