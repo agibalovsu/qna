79dae9182e624704609df72c27014c03f99b15b0
agibalovsu <agibalovsu@mail.ru>
2022-08-28 22:59:05 +0300

    add list users without authenticated user

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -1,5 +1,17 @@
 class Api::V1::ProfilesController < Api::V1::BaseController
+	authorize_resource class: User
+	
+	def index
+		render json: users_without_authenticated_user, each_serializer: ProfileSerializer
+	end
+
 	def me
-		render json: current_resource_owner
+		render json: current_resource_owner, serializer: ProfileSerializer
+	end
+
+	private
+
+	def users_without_authenticated_user
+		@users ||= User.where.not(id: doorkeeper_token.resource_owner_id) if doorkeeper_token
 	end
 end
--- spec/api/v1/profiles_spec.rb
+++ spec/api/v1/profiles_spec.rb
@@ -20,14 +20,36 @@ describe 'Profiles API', type: :request do
 
       it 'returns all public fields' do
         %w[id email admin created_at updated_at].each do |attr|
-          expect(json[attr]).to eq me.send(attr).as_json
+          expect(json['user'][attr]).to eq me.send(attr).as_json
         end
       end
 
       it 'does not return private fields' do
         %w[password encrypted_password].each do |attr|
-          expect(json).to_not have_key(attr)
+          expect(json['user']).to_not have_key(attr)
         end
+      end
+    end
+  end
+
+  describe 'GET /api/v1/profiles' do
+    let(:api_path) { '/api/v1/profiles' }
+
+    it_behaves_like 'API Authorizable' do
+      let(:method) { :get }  
+    end
+
+    context 'authorized' do
+      let(:users) { create_list(:user, 3) }
+      let(:user) { users.first }
+      let(:access_token) { create(:access_token, resource_owner_id: user.id) }
+
+      before { get '/api/v1/profiles', params: { access_token: access_token.token }, headers: headers }
+
+      it_behaves_like 'response success'
+
+      it 'it returns all users, without authenticated user' do 
+        expect(json['users'].size).to eq(users.size - 1) 
       end
     end
   end
