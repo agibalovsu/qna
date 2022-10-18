44e41c59a83cd52aeac97585727f26860ec3d94c
agibalovsu <agibalovsu@mail.ru>
2022-10-19 01:09:18 +0300

    update app/assets/config/manifest.js

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,7 +1,7 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
-//= link_tree ../../../vendor/javascript .js
+//= link_tree ../../../vendor/javascript 
 //= link cocoon.js
 //= link rails-ujs.js
 //= link actioncable.js
