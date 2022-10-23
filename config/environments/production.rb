cdcfbce3f16990ff5caf6d29ac368faa122fe5c2
agibalovsu <agibalovsu@mail.ru>
2022-10-23 19:24:01 +0300

    update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,8 @@ require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_mailer.default_uri_options = { host: "81.163.26.246", protocol: "https"}
+  config.action_mailer.default_url_options = { host: '81.163.26.246' }
+  config.hosts << "81.163.26.246"
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
@@ -76,7 +77,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "qna_production"
 
-  #config.action_mailer.perform_caching = false
+  config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
