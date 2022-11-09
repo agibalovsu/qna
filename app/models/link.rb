f53bf395c4006e30cc68debd740d67b911d46d25
agibalovsu <87718775+agibalovsu@users.noreply.github.com>
2022-11-09 12:45:38 +0300

    Revert "cache"

--- app/models/link.rb
+++ app/models/link.rb
@@ -4,7 +4,7 @@ class Link < ApplicationRecord
   GIST_HOST = 'gist.github.com'
   URL_FORMAT = %r{(^$)|(^(http|https)://[a-z0-9]+([\-.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?/.*)?$)}ix.freeze
 
-  belongs_to :linkable, polymorphic: true, touch: true
+  belongs_to :linkable, polymorphic: true
   validates :name, :url,  presence: true
 
   validates :url, format: { with: URL_FORMAT, message: 'invalid format' }
